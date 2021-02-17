4834f764fbb69e19628cdc3691cf8c8066dd4ffd
Aleksey Vaskin <alzor85@gmail.com>
2021-02-17 17:24:11 +0300

    Adding refactoring

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,20 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/json-placeholder";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value(); //execution
+};
 
 export const fetchPosts = () => {
   return async (dispatch) => {
     const response = await jsonPlaceholder.get("/posts");
+
     dispatch({ type: "FETCH_POSTS", payload: response.data });
   };
 };
@@ -12,3 +24,14 @@ export const fetchUser = (id) => async (dispatch) => {
 
   dispatch({ type: "FETCH_USER", payload: response.data });
 };
+
+// import _ from "lodash";
+// export const fetchUser = (id) => (dispatch) => {
+//   _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`./users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
